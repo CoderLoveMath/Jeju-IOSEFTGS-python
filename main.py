13f1de051e986725b83263987caf5c569272b044
CoderLoveMath <s4103@jejubs.kr>
2020-06-20 15:49:42 +0900

    7. whatisbig triple

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-def whatisbig(n1, n2):
+def whatisbig(n1, n2, n3):
     if n1>n2:
-        return n1
+        if n1>n3:
+            return n1
+        elif n1<n3:
+            return n3
     elif n1<n2:
-        return n2
-    else:
-        return None
+        if n2 > n3:
+            return n2
+        elif n2 < n3:
+            return n3
+    return None
 
 a = int(input('integer: '))
 b = int(input('integer: '))
-print('Big:', whatisbig(a,b))
+c = int(input('integer: '))
+print('Big:', whatisbig(a,b,c))
