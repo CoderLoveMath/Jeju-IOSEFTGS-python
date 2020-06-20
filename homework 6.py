fd6123beab86bd2e38e07cc1ab83322d68055b5b
CoderLoveMath <coderlovemath@gmail.com>
2020-06-20 23:32:37 +0900

    homework 6.py cui finished

--- homework 6.py
+++ homework 6.py
@@ -1,3 +1,12 @@
+# Cannon Simulation ver 1
+# 반갑습니다. 이 시뮬레이션은 물체의 질량, 공기저항을 제외하였을때 (한마디로 우주라고 가정합니다)에서 어떤궤도로 포탄이 날아가는지
+# matplotlib을 이용해 보여주는 프로그램입니다. 코드의 양이 많음에 따라 github 링크로 보내는 점 널리 양해해주시기 바랍니다.
+# How to use
+# 1. parameter을 작성합니다. 첫번째는 속도, 두번째는 각도 (최대 90도이며 오른쪽 기준), 세번째는 유저가 확인할 범위를 말합니다.
+# 2. 팝업창을 확인합니다.
+# 3. 끝! 간단하죠?
+
+
 from math import *
 from matplotlib import pyplot as plt
 import time
