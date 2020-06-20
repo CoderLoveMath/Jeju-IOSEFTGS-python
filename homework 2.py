43ef9cf2b3b5c2859f9a467ea772068c5938dc62
Coder <coderlovemath@gmail.com>
2020-06-20 15:12:48 +0900

    4. minute to many cui and gui

--- homework 2.py
+++ homework 2.py
@@ -1,19 +1,67 @@
 import time
-print("""
- __  __ _             _         _          __  __                   
- |  \/  (_)           | |       | |        |  \/  |                  
- | \  / |_ _ __  _   _| |_ ___  | |_ ___   | \  / | __ _ _ __  _   _ 
- | |\/| | | '_ \| | | | __/ _ \ | __/ _ \  | |\/| |/ _` | '_ \| | | |
- | |  | | | | | | |_| | ||  __/ | || (_) | | |  | | (_| | | | | |_| |
- |_|  |_|_|_| |_|\__,_|\__\___|  \__\___/  |_|  |_|\__,_|_| |_|\__, |
-                                                                __/ |
-                                                               |___/ 
-""")
-base = int(input('minute time: '))
+from tkinter import *
 
-print('converting...')
-time.sleep(2)
-h = base // 60
-m = base % 60
+mode = input('GUI or CUI? gui:1, cui:any(except 1): ')
 
-print('convert finished: ', h, 'hour', m, 'minute')
+def cui():
+    print("""
+     __  __ _             _         _          __  __                   
+     |  \/  (_)           | |       | |        |  \/  |                  
+     | \  / |_ _ __  _   _| |_ ___  | |_ ___   | \  / | __ _ _ __  _   _ 
+     | |\/| | | '_ \| | | | __/ _ \ | __/ _ \  | |\/| |/ _` | '_ \| | | |
+     | |  | | | | | | |_| | ||  __/ | || (_) | | |  | | (_| | | | | |_| |
+     |_|  |_|_|_| |_|\__,_|\__\___|  \__\___/  |_|  |_|\__,_|_| |_|\__, |
+                                                                    __/ |
+                                                                   |___/ 
+    """)
+    base = int(input('minute time: '))
+
+    print('converting...')
+    time.sleep(2)
+    h = base // 60
+    m = base % 60
+
+    print('convert finished: ', h, 'hour', m, 'minute')
+
+def gui():
+    window = Tk()
+    window.title("Minute to Many")
+    window.geometry("640x480+100+100")
+    window.resizable(False, False)
+
+    label = Label(window)
+    label.config(text="Minute to Many")
+
+    spacer = Label(window)
+    spacer.config(text="  ")
+
+    input_e = Entry(window)
+
+    result = Label(window)
+
+    def calc():
+        base = int(input_e.get())
+        h = base // 60
+        m = base % 60
+        result_str = str(h) + 'hour ' + str(m) + 'min'
+        result.config(text=result_str)
+
+    btn = Button(window)
+    btn.config(text="Convert", command=calc)
+
+    label.pack()
+    spacer.pack()
+    input_e.pack()
+    btn.pack()
+    result.pack()
+
+    window.mainloop()
+
+
+if mode == '1':
+    gui()
+else:
+    cui()
+
+
+
