9b15f7fe15aa543d1e6f38de11460b998b7ba4a4
CoderLoveMath <coderlovedev@gmail.com>
2020-06-20 15:20:10 +0900

    just check

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 s = int(input('input second time: '))   # input
-h = s // 360
+h = s // 360    # hoour
 s = s % 360
-m = s // 60
-s = s % 60
+m = s // 60 # minute
+s = s % 60  # second
 
-print(h, 'hour', m, 'min', s, 'sec')
+print(h, 'hour', m, 'min', s, 'sec')    # output
