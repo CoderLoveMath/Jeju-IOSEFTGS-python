6a15331db47b281f6eae032aaa1f55bfe2ac8ffd
CoderLoveMath <coderlovemath@gmail.com>
2020-06-27 15:06:22 +0900

    24. sort!

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import numpy as np
-food = ['짜장면', '짬뽕', '볶음밥']
-print(np.random.choice(food))
-print(np.random.choice(food, 2))
-print(np.random.choice(food, 2, replace=False))
+ds = np.random.randint(1, 101, size=10)
+ds.sort()
+print(ds)
