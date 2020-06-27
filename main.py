12722b0f20da0d5dd91dcb09138eb902fd41e8ca
CoderLoveMath <coderlovemath@gmail.com>
2020-06-27 09:20:50 +0900

    9. for example #2, 1~n sum

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 for i in range(5):
     print(i, end=" ")
+
+print()
+
+# using for statement
+ran = int(input("range: "))
+s = 0
+for i in range(1, ran + 1):
+    s += i
+print('sum is', s)
+
+# using math
+getsum = lambda r: (r + 1) * r / 2
+print('also sum is,', getsum(ran))
