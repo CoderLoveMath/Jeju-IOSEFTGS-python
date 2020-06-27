310b152209ae0a9140b8abadaf18a7a348bc58d9
CoderLoveMath <coderlovemath@gmail.com>
2020-06-27 15:31:12 +0900

    finished homework 14.py

--- homework/homework 14.py
+++ homework/homework 14.py
@@ -1,2 +1,11 @@
 nat = ['Korea', 'United States', 'England', 'Japan', 'China']
-cap = []
+cap = ['Seoul', 'Washington D.C', 'London', 'Dokyo', 'Beijing']
+
+while True:
+    ans = input('Nation name or Exit: ')
+    if ans == "Exit":
+        break
+    elif ans in nat:
+        print(cap[nat.index(ans)])
+    else:
+        print('Anomymous Nation name')
