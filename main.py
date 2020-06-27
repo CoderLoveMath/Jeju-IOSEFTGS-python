7ffdbe2b65a42b44c25f324ac6bb2141315c6899
CoderLoveMath <coderlovemath@gmail.com>
2020-06-27 10:52:48 +0900

    16. 9-number table

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import random
-num = random.randint(1, 20)
-
-while True:
-    select = int(input('Guess my number!'))
-
-    if select is num:
-        print("Right. You're genius!")
-        break
-    elif num < select:
-        print('it is less then your number,', select)
-    else:
-        print('it is greatest then your number,', select)
+for i in range(2, 10):
+    for j in range(2, 10):
+        print(i, '*', j, '=', i * j, end=' ')
+    print()
