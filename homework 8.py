4f76a54719eeef2d57ad3e9889a024330a23b8d7
CoderLoveMath <coderlovemath@gmail.com>
2020-06-27 10:19:05 +0900

    finished homework 8.py

---
+++ homework 8.py
@@ -0,0 +1,15 @@
+import random
+num = random.randint(1, 20)
+try_count = 0
+
+while True:
+    select = int(input('Guess my number!'))
+
+    try_count += 1
+    if select is num:
+        print("Right. You're genius!")
+        break
+    elif num < select:
+        print('it is less then your number,', select)
+    else:
+        print('it is greatest then your number,', select)
