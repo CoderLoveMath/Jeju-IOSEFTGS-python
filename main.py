c0462c481510c87da7dec61dbfb8638497bd6e79
CoderLoveMath <coderlovemath@gmail.com>
2020-06-27 09:25:21 +0900

    10. while example #1

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-for i in range(5):
-    print(i, end=" ")
-
-print()
-
-# using for statement
-ran = int(input("range: "))
 s = 0
-for i in range(1, ran + 1):
-    s += i
-print('sum is', s)
+i = 1
 
-# using math
-getsum = lambda r: (r + 1) * r / 2
-print('also sum is,', getsum(ran))
+while i <= 5:
+    s += i
+    i += 1
+
+print(s)
