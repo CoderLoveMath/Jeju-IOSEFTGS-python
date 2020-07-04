bece2cf2f7b04e26434590027a9b6d52c9fc2d62
CoderLoveMath <coderlovemath@gmail.com>
2020-07-04 09:19:53 +0900

    29. Multidivisor

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = int(input('number: '))
-count = 0
-
-for i in range(1, n+1):
-    if n % i == 0:
-        count += 1
-        print(i, end=' ')
-
-print()
-print('count:', count)
+for n in range(1, 20 + 1):
+    print(n, end=': ')
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i, end=' ')
+    print()
