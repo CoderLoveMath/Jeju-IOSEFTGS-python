32fa7f173530df14ca4fdcabe8af778b40e9bac8
CoderLoveMath <coderlovemath@gmail.com>
2020-07-04 13:56:36 +0900

    36. PalindromeemordnilaP

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-str = input('string: ')
-for i in range(len(str) - 1, -1, -1):
-    print(str[i], end='')
+str1 = input('string: ')
+str2 = ""
+
+for i in range(len(str1) - 1, -1, -1):
+    str2 = str2 + str1[i]
+
+if str1 == str2:
+    print('Palindrome')
+else:
+    print('not Palindrome')
