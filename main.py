47d0ecb9de098c003a3054900d5c17909360e7a5
CoderLoveMath <coderlovemath@gmail.com>
2020-07-04 10:29:29 +0900

    35. Ertos Method (ver 2)

--- main.py
+++ main.py
@@ -13,3 +13,18 @@ for i in range(2, 101):
 for i in range(2, 101):
     if a[i] == 0:
         print(i, end=' ')
+
+print()
+
+# ver 2: use zeros
+import numpy as np
+
+a = np.zeros(101, dtype=int)
+for i in range(2, 101):
+    if a[i] == 0:
+        for j in range(2 * i, 101, i):
+            a[j] = 1
+
+for i in range(2, 101):
+    if a[i] == 0:
+        print(i, end=' ')
