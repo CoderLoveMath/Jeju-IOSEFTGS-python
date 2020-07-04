329acdd47e208b3fc5b07081e26ad3d4978790a3
CoderLoveMath <coderlovemath@gmail.com>
2020-07-04 13:48:41 +0900

    36. gnirts

--- main.py
+++ main.py
@@ -1,30 +1,3 @@
-# ver 1: use list
-a = []
-
-# init
-for i in range(101):
-    a.append(0)
-
-for i in range(2, 101):
-    if a[i] == 0:
-        for j in range(2 * i, 101, i):
-            a[j] = 1
-
-for i in range(2, 101):
-    if a[i] == 0:
-        print(i, end=' ')
-
-print()
-
-# ver 2: use zeros
-import numpy as np
-
-a = np.zeros(101, dtype=int)
-for i in range(2, 101):
-    if a[i] == 0:
-        for j in range(2 * i, 101, i):
-            a[j] = 1
-
-for i in range(2, 101):
-    if a[i] == 0:
-        print(i, end=' ')
+str = input('string: ')
+for i in range(len(str) - 1, -1, -1):
+    print(str[i], end='')
