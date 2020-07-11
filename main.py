0ebd7bfa182708725fde116bc77091fd76c7a1e0
CoderLoveMath <coderlovemath@gmail.com>
2020-07-11 09:23:43 +0900

    43. Flskhu!

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-ch = input('input letter: ')
-key = int(input('distance: '))
+plain = input('Plain Text: ')
+key = 3
+cipher = ''
 
-print(chr(ord('a') + (ord(ch) - ord('a') - key) % 26))
+for ch in plain:
+    if 'a' <= ch <= 'z':
+        cipher += chr(ord('a') + (ord(ch) - ord('a') + key) % 26)
+    elif 'A' <= ch <= 'Z':
+        cipher += chr(ord('A') + (ord(ch) - ord('A') + key) % 26)
+    else:
+        cipher += ch
+
+print('Cipher Text:', cipher)
