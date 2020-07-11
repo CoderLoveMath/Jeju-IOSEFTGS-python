991ba8cf064b91e5b8572a89a6b466a9586cd647
CoderLoveMath <coderlovemath@gmail.com>
2020-07-11 11:36:09 +0900

    49. linear search #2

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import numpy as np
 
-ds = np.random.randint(1, 101, size=100)
+ds = np.random.randint(1, 101, size=100).tolist()
 print(ds)
 key = 1
-found = False
 
-for a in range(len(ds)):
-    if key == ds[a]:
-        found = True
-        print("ds[{a}] is {va}".format(a=a, va=ds[a]))
-        break
+if key in ds:
+    print("ds[{i}] is {v}".format(i=ds.index(key), v=key))
