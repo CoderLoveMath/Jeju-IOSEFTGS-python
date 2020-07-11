86fdc82406d4577a6e976b5c5974f908a076787b
CoderLoveMath <coderlovemath@gmail.com>
2020-07-11 11:46:13 +0900

    50. binary search! Congratulation for 50 main.py commit!

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import numpy as np
 
-ds = np.random.randint(1, 101, size=100).tolist()
-print(ds)
-key = 1
+ds = np.random.randint(1, 5, size=5).tolist()
+key = 3
 
-if key in ds:
-    print("ds[{i}] is {v}".format(i=ds.index(key), v=key))
+for a in range(len(ds) - 1):
+    for b in range(len(ds) - a - 1):
+        if ds[b] > ds[b+1]:
+            ds[b], ds[b+1] = ds[b+1], ds[b]
+
+print(ds)
+
+low = 0
+high = len(ds) - 1
+
+while low <= high:
+    mid = (low + high) // 2
+    if key == ds[mid]:
+        print('ds[{i}] is {v}, high={h} and low={l}'.format(i=mid, v=ds[mid], h=high, l=low))
+        break
+    elif key < ds[mid]:
+        high = mid - 1
+    else:
+        low = mid + 1
