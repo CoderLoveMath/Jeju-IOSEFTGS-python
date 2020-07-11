54eafd0100f24f40ab64d31bcf6b6ff2a211bc74
CoderLoveMath <coderlovemath@gmail.com>
2020-07-11 10:17:27 +0900

    47. Our secret sorting system

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-food = [['사과', 700], ['감귤', 300], ['바나나', 500]]
-
-for a in range(len(food)-2,-1,-1):
-  for b in range(a+1):
-    if food[b + 1][0] < food[b][0]:
-      food[b], food[b+1] = food[b+1], food[b]
-print('가나다순:', end=' ')
-for i in range(len(food)):
-  print(food[i][0], food[i][1], end='  ')
-print()
-
-for a in range(len(food)-2,-1,-1):
-  for b in range(a+1):
-    if food[b][1] < food[b + 1][1]:
-      food[b], food[b+1] = food[b+1], food[b]
-print('가격역순:', end=' ')
-for i in range(len(food)):
-  print(food[i][0], food[i][1], end='  ')
+food = []
+for i in range(2):
+    temp = [input('Name: '), input('Price: ')]
+    food.append(temp)
+print(food)
