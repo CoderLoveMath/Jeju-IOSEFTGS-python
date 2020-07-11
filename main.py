9fafa69745e720c8b2b6ea9f281f084eb42755f2
CoderLoveMath <coderlovemath@gmail.com>
2020-07-11 13:28:28 +0900

    51. my first function

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-import numpy as np
-
-ds = np.random.randint(1, 5, size=5).tolist()
-key = 3
-
-for a in range(len(ds) - 1):
-    for b in range(len(ds) - a - 1):
-        if ds[b] > ds[b+1]:
-            ds[b], ds[b+1] = ds[b+1], ds[b]
-
-print(ds)
-
-low = 0
-high = len(ds) - 1
-
-while low <= high:
-    mid = (low + high) // 2
-    if key == ds[mid]:
-        print('ds[{i}] is {v}, high={h} and low={l}'.format(i=mid, v=ds[mid], h=high, l=low))
-        break
-    elif key < ds[mid]:
-        high = mid - 1
+def diff(a, b):
+    if a > b:
+        return a - b
     else:
-        low = mid + 1
+        return b - a
+
+n1 = int(input('First number: '))
+n2 = int(input('Second number: '))
+print('diff: {}'.format(diff(n1, n2)))
