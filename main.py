fc7f009d8e0d8446dd27bb8baafee423595556f1
CoderLoveMath <coderlovemath@gmail.com>
2020-07-11 10:15:01 +0900

    46. It's very cheap! Buy our sorting system!

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import numpy as np
-ds = np.random.randint(1, 101, size=5)
-print(ds)
+food = [['사과', 700], ['감귤', 300], ['바나나', 500]]
 
-for a in range(0, len(ds) - 1):
-    for b in range(0, len(ds) - a - 1):
-        if ds[b + 1] < ds[b]:
-            ds[b], ds[b+1] = ds[b+1], ds[b]
+for a in range(len(food)-2,-1,-1):
+  for b in range(a+1):
+    if food[b + 1][0] < food[b][0]:
+      food[b], food[b+1] = food[b+1], food[b]
+print('가나다순:', end=' ')
+for i in range(len(food)):
+  print(food[i][0], food[i][1], end='  ')
+print()
 
-print(ds)
+for a in range(len(food)-2,-1,-1):
+  for b in range(a+1):
+    if food[b][1] < food[b + 1][1]:
+      food[b], food[b+1] = food[b+1], food[b]
+print('가격역순:', end=' ')
+for i in range(len(food)):
+  print(food[i][0], food[i][1], end='  ')
